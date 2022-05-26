c78117f6b49fd8eca9ee5ca0a9f7351a9f315fac
Pippen Peng <pippen.peng@woven.com.au>
2022-05-26 12:06:28 +1000

    Quick fix max height for content section

--- js/main.js
+++ js/main.js
@@ -5,14 +5,16 @@ $('#sidebarCollapse').on('click', function() {
 /* Get content max height */
 var navHeight = $('.navbar').height();
 var subnavbarHeight = $('.subnavbar').height();
-var bottomAddRecordHeight = $('.bottom-add-record').height();
+//var bottomAddRecordHeight = $('.bottom-add-record').height();
+var bottomAddRecordHeight = 120;
 var windowHeight = $(window).height();
 var maxcontentinnerHeight = windowHeight - bottomAddRecordHeight - subnavbarHeight - navHeight;
 $('.content-inner').css('max-height', maxcontentinnerHeight);
 $(window).resize(function() {
   var navHeight = $('.navbar').height();
   var subnavbarHeight = $('.subnavbar').height();
-  var bottomAddRecordHeight = $('.bottom-add-record').height();
+  //var bottomAddRecordHeight = $('.bottom-add-record').height();
+	var bottomAddRecordHeight = 120;
   var windowHeight = $(window).height();
   var maxcontentinnerHeight = windowHeight - bottomAddRecordHeight - subnavbarHeight - navHeight;
   $('.content-inner').css('max-height', maxcontentinnerHeight);
